a496d36baa73016f533e146baa62cd5eaa843e59
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-20 12:41:00 -0500

    Update install-jenkins.sh

--- Jenkins/src/install-jenkins.sh
+++ Jenkins/src/install-jenkins.sh
@@ -1,4 +1,6 @@
+# Update linux repository
 sudo yum update -y
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install -y java-1.8.0-openjdk-devel
