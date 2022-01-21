d4dac7fa8bfcd1f803fd3b22f4fd30c96dbe43a2
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-21 10:37:44 -0500

    Update change-resource-tags.sh

--- Tags/change-resource-tags.sh
+++ Tags/change-resource-tags.sh
@@ -1,3 +1,6 @@
+# This script first uses the command aws ec2 describe-instances to return only a list of instance IDs for the development machines that belong to the ERPSystem project. 
+# It then passes those values to the aws ec2 create-tags command, which either creates a new tag or (in this case) overwrites an existing tag.
+
 #!/bin/bash
 
 ids=$(aws ec2 describe-instances --filter "Name=tag:Project,Values=ERPSystem" "Name=tag:Environment,Values=development" --query 'Reservations[*].Instances[*].InstanceId' --output text)
