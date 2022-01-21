5cc54bb85384f56dae2f541dbd81f0fd47eb4831
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-21 10:18:03 -0500

    Update script.sh

--- Tags/script.sh
+++ Tags/script.sh
@@ -2,4 +2,7 @@
 #  The command should output the full set of parameters available for all seven instances that are tagged Project=ERPSystem
 aws ec2 describe-instances --filter "Name=tag:Project,Values=ERPSystem"
 
+# Use the --query parameter to limit the output of the previous command to only the instance ID of the discovered instance:
+aws ec2 describe-instances --filter "Name=tag:Project,Values=ERPSystem" --query 'Reservations[*].Instances[*].InstanceId'
+
 
