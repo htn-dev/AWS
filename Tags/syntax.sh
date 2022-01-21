8338165b073a066fe7bd3ff2bd6633a1c740cd31
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-21 10:28:19 -0500

    Create syntax.sh

---
+++ Tags/syntax.sh
@@ -0,0 +1,6 @@
+# Use the JMESPath syntax by using curly braces to specify a query for multiple properties on each instance returned:
+object.{Alias1:PropertyName1,Alias2:PropertyName2,[…]}
+
+# The value of a specific named tag can be retrieved via a JMESPath query, using the following syntax:
+Tags[?Key==\`Project\`] | [0].Value
+
