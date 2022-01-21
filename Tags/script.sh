1c028ae693cb43c7cae284ae799b72b561638fd4
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-21 10:39:10 -0500

    Update script.sh

--- Tags/script.sh
+++ Tags/script.sh
@@ -17,6 +17,8 @@ aws ec2 describe-instances --filter "Name=tag:Project,Values=ERPSystem" --query 
 # Add a second tag filter to see only the instances associated with the project named ERPSystem that belong to the Environment named development:
 aws ec2 describe-instances --filter "Name=tag:Project,Values=ERPSystem" "Name=tag:Environment,Values=development" --query 'Reservations[*].Instances[*].{ID:InstanceId,AZ:Placement.AvailabilityZone,Project:Tags[?Key==`Project`] | [0].Value,Environment:Tags[?Key==`Environment`] | [0].Value,Version:Tags[?Key==`Version`] | [0].Value}'
 
-# Open the file /home/ec2-user/change-resource-tags.sh
-nano change-resource-tags.sh
+# Run the file /home/ec2-user/change-resource-tags.sh
+./change-resource-tags.sh
 
+# Verify that the version number on these instances has been incremented and that other non-development boxes in the ERPSystem project have been unaffected
+aws ec2 describe-instances --filter "Name=tag:Project,Values=ERPSystem" --query 'Reservations[*].Instances[*].{ID:InstanceId, AZ:Placement.AvailabilityZone, Project:Tags[?Key==`Project`] |[0].Value,Environment:Tags[?Key==`Environment`] | [0].Value,Version:Tags[?Key==`Version`] | [0].Value}'
