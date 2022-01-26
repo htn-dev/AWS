e9c2392dc178ccd35d749775520610f1c3f67843
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-26 10:52:45 -0500

    Update retrieve-info.sh

--- Migrate to RDS/retrieve-info.sh
+++ Migrate to RDS/retrieve-info.sh
@@ -2,8 +2,4 @@
 # Use an AWS CLI command that also returns the VPC ID of its VPC, and the Group ID of its security group
 
 
-aws ec2 describe-instances \
-
---filters "Name=tag:Name,Values= CafeInstance" \
-
---query "Reservations[*].Instances[*].[InstanceId,InstanceType,PublicDnsName,PublicIpAddress,Placement.AvailabilityZone,VpcId,SecurityGroups[*].GroupId]"
+aws ec2 describe-instances --filters "Name=tag:Name,Values= CafeInstance" --query "Reservations[*].Instances[*].[InstanceId,InstanceType,PublicDnsName,PublicIpAddress,Placement.AvailabilityZone,VpcId,SecurityGroups[*].GroupId]"
