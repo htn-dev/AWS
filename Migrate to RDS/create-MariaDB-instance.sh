c1fb15ee11913c5c8c32dace7a48b120635654aa
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-26 13:11:29 -0500

    Create create-MariaDB-instance.sh

---
+++ Migrate to RDS/create-MariaDB-instance.sh
@@ -0,0 +1,35 @@
+'''     
+    DB instance identifier: CafeDBInstance
+    Engine option: MariaDB
+    DB engine version: 10.2.11
+    DB instance class: db.t3.micro
+    Allocated storage: 20 GB
+    Availability Zone: CafeInstance Availability Zone
+    DB Subnet group: CafeDB Subnet Group
+    VPC security groups: CafeDatabaseSG
+    Public accessibility: No
+    Username: root
+    Password: Re:Start!9
+'''
+
+aws rds create-db-instance \
+
+--db-instance-identifier CafeDBInstance \
+
+--engine mariadb \
+
+--engine-version 10.2.11 \
+
+--db-instance-class db.t3.micro \
+
+--allocated-storage 20 \
+
+--availability-zone <CafeInstance Availability Zone> \
+
+--db-subnet-group-name "CafeDB Subnet Group" \
+
+--vpc-security-group-ids <CafeDatabaseSG Group ID> \
+
+--no-publicly-accessible \
+
+--master-username root --master-user-password 'Re:Start!9'
