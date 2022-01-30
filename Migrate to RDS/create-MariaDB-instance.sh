a19dbcff5bb7ae0abee9c93ceafb2e6d990d136f
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-30 15:18:42 -0500

    Update create-MariaDB-instance.sh

--- Migrate to RDS/create-MariaDB-instance.sh
+++ Migrate to RDS/create-MariaDB-instance.sh
@@ -38,6 +38,10 @@ aws rds create-db-instance \
 --master-username root --master-user-password 'Re:Start!9'
 
 
+
+aws rds create-db-instance --db-instance-identifier CafeDBInstance --engine mariadb --engine-version 10.2.11 --db-instance-class db.t3.micro --allocated-storage 20 --availability-zone <CafeInstance Availability Zone> --db-subnet-group-name "CafeDB Subnet Group" --vpc-security-group-ids <CafeDatabaseSG Group ID> --no-publicly-accessible --master-username root --master-user-password 'Re:Start!9'
+
+
 ''' Monitor the status of the database instance. This command displays the status of the database instance as the last returned value.  It also shows: 
 
     Endpoint address
