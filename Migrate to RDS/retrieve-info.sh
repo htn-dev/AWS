cb61617712f87af877af8bd9f0d4b70e7fe19319
HTN <96134288+htn-dev@users.noreply.github.com>
2022-02-01 19:13:52 -0500

    Update retrieve-info.sh

--- Migrate to RDS/retrieve-info.sh
+++ Migrate to RDS/retrieve-info.sh
@@ -1,4 +1,4 @@
-# Determine the Instance ID, Instance Type, Public DNS name, Public IP address, and Availability Zone of the CafeInstance.
+# Determine the Instance ID, Instance Type, Public DNS name, Public IP address, and Availability Zone of the CafeInstance
 # Use an AWS CLI command that also returns the VPC ID of its VPC, and the Group ID of its security group
 
 aws ec2 describe-instances --filters "Name=tag:Name,Values= CafeInstance" --query "Reservations[*].Instances[*].[InstanceId,InstanceType,PublicDnsName,PublicIpAddress,Placement.AvailabilityZone,VpcId,SecurityGroups[*].GroupId]"
