3236b1ecd2ffdda92c52c544c129442f951ec562
HTN <96134288+htn-dev@users.noreply.github.com>
2022-02-01 19:14:57 -0500

    Update install-git.sh

--- Jenkins/src/install-git.sh
+++ Jenkins/src/install-git.sh
@@ -1,7 +1,7 @@
 # Perform a quick update on your instance:
 sudo yum update -y
  
-#Install git in your EC2 instance
+# Install git in your EC2 instance
 sudo yum install git -y
  
 #Check git version
