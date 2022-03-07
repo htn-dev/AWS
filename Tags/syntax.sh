104905201894ec73a28137e09204f4b296a8b26f
HTN <96134288+htn-dev@users.noreply.github.com>
2022-03-07 16:28:47 -0500

    Update syntax.sh

--- Tags/syntax.sh
+++ Tags/syntax.sh
@@ -1,4 +1,4 @@
-# Use the JMESPath syntax by using curly braces to specify a query for multiple properties on each instance returned:
+# Use the JMESPath syntax by using curly braces to specify a query for multiple properties on each instance returned
 object.{Alias1:PropertyName1,Alias2:PropertyName2,[…]}
 
 # The value of a specific named tag can be retrieved via a JMESPath query, using the following syntax:
