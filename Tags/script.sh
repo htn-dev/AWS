ee5b507d52e47abfa7786065ce266bc5f202a413
HTN <96134288+htn-dev@users.noreply.github.com>
2022-03-07 16:27:56 -0500

    Update script.sh

--- Tags/script.sh
+++ Tags/script.sh
@@ -1,4 +1,5 @@
 #  Find all instances in your account that are tagged with a tag of Project and a value of ERPSystem.
+
 #  The command should output the full set of parameters available for all seven instances that are tagged Project=ERPSystem
 aws ec2 describe-instances --filter "Name=tag:Project,Values=ERPSystem"
 
