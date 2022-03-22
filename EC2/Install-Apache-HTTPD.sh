faf6b7fa1622fc4f8ca141f50ca31923bd5ecde3
HTN <96134288+htn-dev@users.noreply.github.com>
2022-03-22 19:39:53 -0400

    Update Install-Apache-HTTPD.sh

--- EC2/Install-Apache-HTTPD.sh
+++ EC2/Install-Apache-HTTPD.sh
@@ -9,4 +9,5 @@ lynx http://localhost
 
 # You will initially see an error in the browser that reads HTTP 301 - Forbidden
 # This is the default response for new installations of Apache httpd
+# Don't worry - this is expected and will show the Apache test page after a few seconds
 # Type q to exit Lynx
