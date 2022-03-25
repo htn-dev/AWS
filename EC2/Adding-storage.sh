8d1fd2e92014861214f715857ccb3bdc88b8379c
HTN <96134288+htn-dev@users.noreply.github.com>
2022-03-24 20:42:08 -0400

    Update Adding-storage.sh

--- EC2/Adding-storage.sh
+++ EC2/Adding-storage.sh
@@ -3,3 +3,13 @@
 # The EBS volume is attached to the EC2 instance. However, you need to make the volume available to use by formatting and mounting it
 
 ## FORMAT the volume with mkfs
+
+sudo mkdir /data
+
+# mount the volume at the directory
+sudo mount /dev/xvdf /data
+
+# create a backup of the /etc/fstab file.
+sudo cp /etc/fstab /etc/fstab.orig
+
+# Next, find the device’s 128-bit universally unique identifier (UUID). This UUID, unlike the devices’ names, is unique and does not change
