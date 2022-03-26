14d19ea69103c67cad948333190f6756be0bffc7
HTN <96134288+htn-dev@users.noreply.github.com>
2022-03-26 19:10:06 -0400

    Update Adding-storage.sh

--- EC2/Adding-storage.sh
+++ EC2/Adding-storage.sh
@@ -1,6 +1,5 @@
 # At first, EBS volumn needed to be created and attached to an EC2 instance on the AWS console
 # The EBS volume is attached to the EC2 instance. However, you need to make the volume available to use by formatting and mounting it
-
 # FORMAT the volume with mkfs
 
 sudo mkdir /data
