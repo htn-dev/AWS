58c63570fe8a641a2232ea239c1d275da8e742e8
HTN <96134288+htn-dev@users.noreply.github.com>
2022-03-29 18:54:30 -0400

    Update Moving-the-web-root.sh

--- EC2/Moving-the-web-root.sh
+++ EC2/Moving-the-web-root.sh
@@ -1,2 +1,6 @@
 # This file shows how to move the web root of Apache server to the new volume.
 
+# Move the document root directory’s content from /var/www/html to the new mount point directory /data.
+# Copy the document root directory’s content to the new location using rsync:
+sudo rsync -av /var/www/html /data
+
