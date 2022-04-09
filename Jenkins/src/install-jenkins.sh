78c622bc2dcbd58d9c93d978bde448c06cdf419b
HTN <96134288+htn-dev@users.noreply.github.com>
2022-04-08 23:59:37 -0400

    Update install-jenkins.sh

--- Jenkins/src/install-jenkins.sh
+++ Jenkins/src/install-jenkins.sh
@@ -4,6 +4,7 @@ sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install -y java-1.8.0-openjdk-devel
+
 # repository that provides 'daemonize'
 sudo amazon-linux-extras install epel -y
 
